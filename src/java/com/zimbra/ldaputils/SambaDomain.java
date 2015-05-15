92d01cf0537e771132f89e1c89dcfc86597b165d
Gren Elliot <gren.elliot@zimbra.com>
2015-05-15 04:06:48 -0500

    Bug:80452 cache freshness
    Track more dependencies for freshness in LDAP caches
    
    NamedEntry changed to implement Comparable<NamedEntry> instead of just Comparable which means that some of the collection sorts don't throw up warnings in Eclipse any more.  Part of this involved removing duplicate compareTo methods from other classes.
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=80452
    
    Copied from Perforce
     Change: 544751

--- src/java/com/zimbra/ldaputils/SambaDomain.java
+++ src/java/com/zimbra/ldaputils/SambaDomain.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -18,7 +18,6 @@ package com.zimbra.ldaputils;
 
 import java.util.Map;
 
-import com.zimbra.cs.account.NamedEntry;
 import com.zimbra.cs.ldap.LdapException;
 import com.zimbra.cs.ldap.ZAttributes;
 /**
@@ -27,27 +26,22 @@ import com.zimbra.cs.ldap.ZAttributes;
 public class SambaDomain extends LDAPUtilEntry {
 
 	private static final String A_sambaSID = "sambaSID";
-	private static final String A_sambaDomainName = "sambaDomainName";	
-	
-	public SambaDomain(String dn, ZAttributes attrs, Map<String, Object> defaults) 
+	private static final String A_sambaDomainName = "sambaDomainName";
+
+	public SambaDomain(String dn, ZAttributes attrs, Map<String, Object> defaults)
 	throws LdapException {
         super(dn, attrs, defaults);
         mName = attrs.getAttrString(A_sambaSID);
         mId = attrs.getAttrString(A_sambaDomainName);
     }
 
+    @Override
     public String getId() {
         return getAttr(A_sambaSID);
     }
 
+    @Override
     public String getName() {
         return getAttr(A_sambaDomainName);
-    }
-
-    public int compareTo(Object obj) {
-        if (!(obj instanceof NamedEntry))
-            return 0;
-        NamedEntry other = (NamedEntry) obj;
-        return getName().compareTo(other.getName());
     }
 }
